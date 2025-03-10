9b01381f5953f2b5c32aeb5d1391e6432dec2635
Taief HILALI <taief.hilali@esprit.tn>
2025-03-11 00:52:55 +0100

    ADD NEW COLORS.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,17 +9,20 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Taief Hilali</h1>
         <p className={styles.description}>
-        I'm a Software Engineer and Full-Stack Developer with almost 2 years of experience building dynamic and scalable web applications using React.js and Node.js(MERN STACK). I specialize in crafting intuitive user interfaces, optimizing backend performance, and creating seamless integrations. Let’s connect and bring great ideas to life! 🚀
-        </p>
+  I'm a Software Engineer and Full-Stack Developer with almost 2 years of experience building dynamic and scalable web applications using React.js and Node.js(MERN STACK). I specialize in crafting intuitive user interfaces, optimizing backend performance, and creating seamless integrations. Let’s connect and bring great ideas to life! 🚀
+</p>
+<br />
+<br />
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/taief.png")}
-        alt="Hero image of me"
-        className={styles.heroImg}
-      />
+  src={getImageUrl("hero/taief.png")}
+  alt="Hero image of me"
+  className={styles.heroImg}
+/>
+
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
